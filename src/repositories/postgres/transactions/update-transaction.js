db59edb3851ae9748eac2709f4dc5a35eedfcb60
guitessmann09 <tessmanng9@gmail.com>
2024-11-20 16:42:21 -0300

    feat: add route to update a transaction

--- src/repositories/postgres/transactions/update-transaction.js
+++ src/repositories/postgres/transactions/update-transaction.js
@@ -1,19 +1,19 @@
 import { PostgresHelper } from '../../../db/postgres/helper.js'
 
 export class PostgresUpdateTransactionRepository {
-    async execute(transactionId, updateTransactionParamns) {
+    async execute(transactionId, updateTransactionParams) {
         const updateFields = []
         const updateValues = []
 
-        Object.keys(updateTransactionParamns).forEach((key) => {
+        Object.keys(updateTransactionParams).forEach((key) => {
             updateFields.push(`${key} = $${updateValues.length + 1}`)
-            updateValues.push(updateTransactionParamns[key])
+            updateValues.push(updateTransactionParams[key])
         })
 
         updateValues.push(transactionId)
 
         const updateQuery = `
-            UPDATE users
+            UPDATE transactions
             SET ${updateFields.join(', ')}
             WHERE id = $${updateValues.length}
             RETURNING *
